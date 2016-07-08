b5b99b7e18ec30ad84222b0f036e7aaba5005154
jayrogers <jayrogers@jays-mbp.att.net>
2016-07-08 02:43:22 -0400

    Add comments

--- src/Reverse.java
+++ src/Reverse.java
@@ -5,21 +5,22 @@ public class Reverse {
 
 		Scanner input = new Scanner(System.in);
 			
-		System.out.println("Enter Number Below: ");
-		System.out.println("=======================");
+		   System.out.println("Enter Number Below: ");
+		   System.out.println("======================="); // line isn't necessary for code, but adds a visual element
 		
 		int number = input.nextInt();
 		int rev = 0;
 	
-		while (number > 0) {
-		
-			rev = rev * 10 + (number) % 10;
-			number = (number / 10);
+		while (number != 0) {
+		//The while loop seems more convenient in situations where the input integer's size is unknown, versus for loops 
+			
+			rev = rev * 10 + (number) % 10; 
+			number = (number / 10);  
 		
 		}
-//Adjust System.out.println in to receive only the final print out, instead of 1/loop.
-			System.out.println(rev);
+		//Adjust System.out.println to receive only the final integer, instead of 1 per loop.
 		
+			System.out.println(rev);
 	}
 	
 }
