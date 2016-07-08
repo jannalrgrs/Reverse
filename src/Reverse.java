2336985b2e952e70101597e6f849d02c5f365721
jannalrgrs <jannalrgrs@gmail.com>
2016-07-08 07:17:23 -0400

    Update Reverse.java

--- src/Reverse.java
+++ src/Reverse.java
@@ -6,23 +6,22 @@ public class Reverse {
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("Enter Number to Reverse: ");
-		System.out.println("======================="); // line isn't necessary, but adds a visual element
+		System.out.println("======================="); // Line isn't necessary, but adds a visual element
 							
 		int number = input.nextInt();
 		int rev = 0;
 		while (number > 0) {  		 /* The while loop seems more convenient in
-								  	  *situations where the input integer's size is unknown,
-								  	  * versus for loops.
-								  	  */
-			
+						  * situations where the input integer's size is unknown,
+						  * versus for loops. */
+								  	  
 			rev = rev * 10 + (number) % 10;
 			number = number / 10;	 // Number result will always be a whole number, due to the data type int. 
 																			
 		}
 
-		System.out.println(rev);   /* Inside curly brackets of while loop, System.out.println(rev)
-		 						      *is executed as many times as the loop runs. Slowly builds to the
-		 						      *final reversed number*/									 	
+		System.out.println(rev);        /* Inside curly brackets of while loop, System.out.println(rev)
+ 					         * is executed as many times as the loop runs. Slowly builds to the
+		 			         * final reversed number*/									 	
 
 	}
 	
